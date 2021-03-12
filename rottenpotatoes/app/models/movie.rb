0f6104c4fa1fd89e999d6b272bd98d8edb7c6272
YCWangVince <wyc2016@mail.ustc.edu.cn>
2021-03-12 01:29:32 +0000

    work for new notification

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,4 +1,13 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+    %w(G PG PG-13 NC-17 R)
+    end
+    # def self.with_ratings(ratings_list)
+    #     if ratings_list.nil? or ratings_list.empty?
+    #       return self.all
+    #     end
+    #     where({rating: ratings_list})
+    # end
     def others_by_same_director(director)
         Movie.where(director: director)
     end
